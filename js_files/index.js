e5002ec2542ef9f763dc7377ae432821a0e22851
liangl5 <liangl5@miamioh.edu>
2022-03-02 17:32:29 -0500

    extra functionality

--- js_files/index.js
+++ js_files/index.js
@@ -1,6 +1,4 @@
-document.getElementById("searchBtn").addEventListener("click", (evt)=>{
-    console.log(evt)
-})
+
 
 
 // This part initally sets up event listeners to the nav bar and will correctly change color
@@ -15,12 +13,34 @@ for (const element of navBtns) {
             if (element.innerText == evt.target.innerText) {
                 element.classList.add("active")
                 element.classList.remove("notactive")
+
+                //console.log(element.innerText + "Div")
+                document.getElementById(element.innerText + "Div").style.display = "block"
+
             } else {
                 element.classList.add("notactive")
                 element.classList.remove("active")
+
+                //console.log(element.innerText + "Div")
+                document.getElementById(element.innerText + "Div").style.display = "none"
             }
         }
 
-        // can add some changing parts here that unhides data
     })
 }
+
+
+
+document.getElementById("searchBtn").addEventListener("click", (evt)=>{
+    var strInput = document.getElementById("searchBar").value
+
+    // simple check to make sure that not just the input string
+    if (strInput != "") { 
+        // simulate clicking so it will return to home page
+        var displayDiv = document.getElementById("homeNav")
+        displayDiv.click()
+
+        // super simple update which we will hook up to actual querying
+        document.getElementById("HomeDiv").innerText = strInput
+    }
+})
