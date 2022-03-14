d692d1c31bf01ad4d2a9cab30d592a767b98688a
liangl5 <liangl5@miamioh.edu>
2022-03-13 22:26:47 -0400

    switch between login and create acc

---
+++ js_files/login.js
@@ -0,0 +1,12 @@
+
+
+
+document.getElementById("createAccBtn").addEventListener("click", ()=>{
+    document.getElementById("createAccDiv").style.display = "block";
+    document.getElementById("loginDiv").style.display = "none";
+})
+
+document.getElementById("loginBtn").addEventListener("click", ()=>{
+    document.getElementById("createAccDiv").style.display = "none";
+    document.getElementById("loginDiv").style.display = "block";
+})
