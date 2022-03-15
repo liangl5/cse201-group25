0ff45bcd1f979cbbe1b9d67b314956799d8d9467
liangl5 <liangl5@miamioh.edu>
2022-03-15 16:41:04 -0400

    display

--- js_files/index.js
+++ js_files/index.js
@@ -1,4 +1,11 @@
-
+// grab the app data
+// can connect to db later
+globalAppData =  [
+            ['Uber', 'Uber Technologies', 'Travel', 'Peer-to-peer ridesharing and food delivery platform'], 
+            ['Trivago', 'Trivago Inc', 'Travel', 'Hotel: Trivago'],
+            ['Raid Shadow Legends', 'Gamers International', 'Games', 'Bet you never saw one of our ads'],
+            ['Minecraft', 'Mojang', 'Games', 'The best mining game']
+        ]
 
 
 // This part initally sets up event listeners to the nav bar and will correctly change color
@@ -34,19 +41,31 @@ for (const element of navBtns) {
 document.getElementById("searchBtn").addEventListener("click", (evt)=>{
     var strInput = document.getElementById("searchBar").value
 
-    // simple check to make sure that not just the input string
-    if (strInput != "") { 
-        // simulate clicking so it will return to home page
-        var displayDiv = document.getElementById("homeNav")
-        displayDiv.click()
+    // simulate clicking so it will return to home page
+    var displayDiv = document.getElementById("homeNav")
+    displayDiv.click()
 
-        // super simple update which we will hook up to actual querying
-        document.getElementById("HomeDiv").innerText = strInput
+    // super simple update which we will hook up to actual querying
+    // document.getElementById("HomeDiv").innerText = strInput
+    displayApps = globalAppData
+    if (strInput != "") {
+        // somefiltering here
+        console.log("filter here")
     }
+
+    // inner html
+    displayHTML = ""
+    for (i = 0; i < displayApps.length; i++) {
+        displayHTML += "<div class='displayApps'>"
+        displayHTML += displayApps[i][0]
+
+        displayHTML += "</div>"
+    }
+
+    document.getElementById("HomeDiv").innerHTML = displayHTML
 })
 
 
-// the login functionality
-document.getElementById("loginBtn").addEventListener("click", (evt)=>{
-    console.log(evt)
-})
+// Initialize the click
+document.getElementById("searchBtn").click();
+
